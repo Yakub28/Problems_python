865c5a81850d889c26ce9b4adfd52f725cf4bd2a
Yakub <101415633+Yakub28@users.noreply.github.com>
2022-11-26 13:53:32 +0100

    Add files via upload

---
+++ turtle_graphics.py
@@ -0,0 +1,10 @@
+Traceback (most recent call last):
+  File "main.py", line 1, in <module>
+    import turtle
+  File "/usr/lib/python3.8/turtle.py", line 107, in <module>
+    import tkinter as TK
+ModuleNotFoundError: No module named 'tkinter'
+
+
+** Process exited - Return Code: 1 **
+Press Enter to exit terminal
